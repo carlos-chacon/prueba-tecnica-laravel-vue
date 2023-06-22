c325d1e2995501cee3f7adcb61fdf17397a9c19c
carlos <ccarch81@gmail.com>
2023-06-21 21:41:23 -0500

    21062023

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,77 +6,18 @@ import DropdownLink from "@/Components/DropdownLink.vue";
 import NavLink from "@/Components/NavLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/vue3";
+import MenuLayout from "./MenuLayout.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <NavLink
-                            class="nav-link"
-                            :href="route('dashboard')"
-                            :active="route().current('dashboard')"
-                        >
-                            Dashboard
-                        </NavLink>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a
-                            class="nav-link dropdown-toggle"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            {{ $page.props.auth.user.name }}
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <NavLink
-                                    class="dropdown-item"
-                                    :href="route('profile.edit')"
-                                    :active="route().current('profile.edit')"
-                                >
-                                    Edit
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    class="dropdown-item"
-                                    :href="route('logout')"
-                                    method="post"
-                                    as="button"
-                                >
-                                    Log Out
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <MenuLayout />
 
-    <div class="min-h-screen bg-gray-100">
+    <div class="min-vw-100 bg-gray-100">
         <!-- Page Heading -->
         <header v-if="$slots.header" class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="max-w-7xl py-3 mx-auto px-4 sm:px-5 lg:px-4">
                 <slot name="header" />
             </div>
         </header>
---
+++ resources/js/Layouts/MenuLayout.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import NavLink from "@/Components/NavLink.vue";
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark py-3">
+        <div class="container-fluid ">
+            <a class="navbar-brand text-white" href="#">PHP - Vue</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <hr class="d-lg-none text-white-50" />
+                <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav pe-5">
+                    <li class="nav-item">
+                        <NavLink
+                            class="nav-link text-white"
+                            :href="route('dashboard')"
+                            :active="route().current('dashboard')"
+                        >
+                            Dashboard
+                        </NavLink>
+                    </li>
+                </ul>
+                <hr class="d-lg-none text-white-50" />
+                <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                    <li class="nav-item dropdown ">
+                        <a
+                            class="nav-link dropdown-toggle text-white"
+                            href="#"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            {{ $page.props.auth.user.name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <NavLink
+                                    class="dropdown-item"
+                                    :href="route('profile.edit')"
+                                    :active="route().current('profile.edit')"
+                                >
+                                    Edit
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    class="dropdown-item"
+                                    :href="route('logout')"
+                                    method="post"
+                                    as="button"
+                                >
+                                    Log Out
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
